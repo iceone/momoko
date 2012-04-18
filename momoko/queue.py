04b4ef7168ac056a9a40d72155d9e4c82ed475d0
Ilnur Ibragimov <iceone7@gmail.com>
2012-04-18 13:31:07 +0600

    Query Queue

---
+++ momoko/queue.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+import tornado
+from tornado.ioloop import PeriodicCallback
+import time
+import functools
+from momoko.clients import AdispClient, AsyncClient
+import uuid
+from collections import OrderedDict
+from psycopg2.extensions import adapt
+from tornado import gen
+
+class DbQueryQueue(object):
+
+    def __init__(self, db, ioloop, poll_timeout=0.5, queue_length=5, noresult_poll_timeout=1, noresult_queue_length=5000):
+        self.db = db
+
+        if isinstance(self.db, AdispClient):
+            self._execute = super(AdispClient, self.db).execute
+            self._batch = super(AdispClient, self.db).batch
+        else:
+            self._execute = self.db.execute
+            self._batch = self.db.batch
+
+        self.ioloop = ioloop
+
+        self.poll_timeout = poll_timeout
+        self.queue = OrderedDict()
+        self.queue_length = queue_length
+        self.queue_poll_handler = None
+
+        self.noresult_queue = OrderedDict()
+        self.noresult_poll_timeout = noresult_poll_timeout
+        self.noresult_queue_dumper = PeriodicCallback(self.noresult_timeout_check, self.noresult_poll_timeout * 1000, io_loop=self.ioloop)
+        self.noresult_queue_length = noresult_queue_length
+        self.noresult_last_time = None
+
+    def start(self):
+        '''
+        Start queue polling
+        '''
+        self.noresult_last_time = time.time()
+        self.noresult_queue_dumper.start()
+        self.queue_poll_handler = self.ioloop.add_timeout(time.time()+self.poll_timeout, self.poller)
+
+    def stop(self):
+        '''
+        Stop queue polling
+        '''
+        self.noresult_queue_dumper.stop()
+        if self.queue_poll_handler:
+            self.ioloop.remove_timeout(self.queue_poll_handler)
+
+    def format_sql(self, sql_tmpl, params=None):
+        if params:
+            if isinstance(params, dict):
+                adapted = {}
+                for k in params.keys():
+                    adapted[k] = adapt(params[k]).getquoted()
+                sql = sql_tmpl % adapted
+            elif isinstance(params, (list, tuple)):
+                adapted = []
+                for p in params:
+                    adapted.append(adapt(p).getquoted())
+                adapted = tuple(adapted)
+            sql = sql_tmpl % adapted
+        else:
+            sql = sql_tmpl
+        return sql
+
+    def fetchone(self, sql_tmpl, params=None, callback=None):
+        self.execute(sql_tmpl, params, command='fetchone', callback=callback)
+
+    def fetchall(self, sql_tmpl, params=None, callback=None):
+        self.execute(sql_tmpl, params, command='fetchall', callback=callback)
+
+    def execute(self, sql_tmpl, params=None, command='', callback=None):
+        assert command in ('fetchall', 'fetchone')
+        sql = self.format_sql(sql_tmpl, params)
+        key = str(uuid.uuid4())
+        self.queue[key] = ((sql, callback, key, command))
+
+    @gen.engine
+    def poller(self):
+        i = 0
+        self.temp_queries = OrderedDict()
+
+        while self.queue and i < self.queue_length:
+            i += 1
+            uid, item = self.queue.popitem(last=False)  # FIFO
+            self.temp_queries[uid] = item
+
+        if not self.temp_queries:
+            self.queue_poll_handler = self.ioloop.add_timeout(time.time()+self.poll_timeout, self.poller)
+            return
+
+        keys = []
+        for sql, callback, uid, command in self.temp_queries.values():
+            keys.append(uid)
+            self._execute(sql, callback=(yield gen.Callback(uid)))
+
+        cursors = yield gen.WaitAll(keys)
+
+        for uid, cursor in zip(keys, cursors):
+            _, callback, __, command = self.temp_queries.pop(uid)
+            try:
+                data = getattr(cursor, command)()
+                callback(data)
+            except Exception as e:
+                print 'DBQUERY-QUEUE:ERROR:', e.message
+
+        # if queue is not empty - call poller without timeout
+        if self.queue:
+            self.ioloop.add_callback(self.poller)
+        else:
+            self.queue_poll_handler = self.ioloop.add_timeout(time.time()+self.poll_timeout, self.poller)
+
+    def execute_noresult(self, sql_tmpl, params=None, callback=None):
+        '''
+        Execute query without result
+        :param sql_tmpl: Template of SQL query of SQL query if params is None.
+        :param params: Tuple, list or dict of params.
+        :param callback: Callback to execute after query is complete.
+        '''
+        sql = self.format_sql(sql_tmpl, params)
+        key = str(uuid.uuid4())
+        self.noresult_queue[key] = (sql, callback)
+
+        if len(self.noresult_queue) >= self.noresult_queue_length:
+            self.ioloop.add_callback(self.execute_noresult_queue)
+
+    def noresult_timeout_check(self):
+        '''
+        Check expiry of queue.
+        '''
+        if (time.time() - self.noresult_last_time) > self.noresult_poll_timeout:
+            self.execute_noresult_queue()
+
+    def execute_noresult_queue(self):
+        '''
+        Executes noresult queue in single query.
+        After executing performs all callbacks.
+        '''
+        if not self.noresult_queue:
+            return
+
+        callbacks = []
+        sql_defs = []
+        while self.noresult_queue:
+            uid, item = self.noresult_queue.popitem(last=False)
+            sql, callback = item
+            sql_defs.append(sql)
+            if callback:
+                callbacks.append(callback)
+        sql = ';\n'.join(sql_defs)
+
+        callback = functools.partial(self.perform_callbacks, callbacks=callbacks)
+        self._execute(sql, callback=callback)
+
+        self.noresult_last_time = time.time()
+
+    def perform_callbacks(self, cursor, callbacks=None):
+        for callback in callbacks:
+            callback()
---
+++ tests/queue.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+import unittest
+import os, sys
+import time
+
+import momoko
+from momoko.adisp import process, async
+from tornado.testing import AsyncTestCase
+from tornado import ioloop
+
+from momoko.queue import DbQueryQueue
+
+import settings
+
+
+class DbQueueTestCase(AsyncTestCase):
+
+    def setUp(self):
+        super(DbQueueTestCase, self).setUp()
+
+        self.db = momoko.AsyncClient({
+            'host': settings.host,
+            'port': settings.port,
+            'database': settings.database,
+            'user': settings.user,
+            'password': settings.password,
+            'min_conn': settings.min_conn,
+            'max_conn': settings.max_conn,
+            'cleanup_timeout': settings.cleanup_timeout,
+            'ioloop': self.io_loop
+        })
+
+        self.db_queue = DbQueryQueue(self.db, self.io_loop, poll_timeout=0.2, queue_length=2,
+                                     noresult_queue_length=10000)
+        self.db_queue.start()
+
+    def tearDown(self):
+        self.db_queue.stop()
+        super(DbQueueTestCase, self).tearDown()
+
+    def test_format_sql(self):
+        sql = self.db_queue.format_sql('SELECT %s, %s', (1, 2))
+        self.assertEqual(sql, 'SELECT 1, 2')
+
+        sql = self.db_queue.format_sql('SELECT %(one)s, %(two)s', {'one': 1, 'two': 2})
+        self.assertEqual(sql, 'SELECT 1, 2')
+
+    def test_fetchall(self):
+        expected = []
+
+        def after_fetchall(data):
+            self.assertEqual(data[0][0], expected.pop(0))
+            if not expected:
+                self.stop()
+
+        for i in xrange(0, 100):
+            expected.append(i)
+            self.db_queue.fetchall('SELECT %s', (i,), callback=after_fetchall)
+
+        self.wait()
+
+    def test_query_without_result(self):
+        sql = ''
+        expected = []
+
+        def after_execute():
+            expected.pop(0)
+            if not expected:
+                self.stop()
+
+        for i in xrange(0, 100):
+            sql += 'SELECT %d;\n' % i
+            expected.append(i)
+            self.db_queue.execute_noresult(sql, callback=after_execute)
+
+        self.wait(timeout=10)
+
+if __name__ == '__main__':
+    unittest.main()
